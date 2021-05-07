c925687bdcd4ecb7864da59d9b128c1005f49821
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-05-07 11:52:33 -0400

    html route change

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.get("/", (req, res) => {
-  console.log("here?");
-  res.sendFile(path.join(__dirname, "/index.html"));
-});
+// router.get("/", (req, res) => {
+//   console.log("here?");
+//   res.sendFile(path.join(__dirname, "/index.html"));
+// });
 
 module.exports = router;
